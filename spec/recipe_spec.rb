91df10e3b92e4fd5a80aced292bae9c06da2bb64
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-04-12 11:40:57 +0800

    add last specs

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -3,4 +3,5 @@ require('spec_helper')
 describe(Recipe) do
   it { should have_many(:instructions) }
   it { should have_many(:ingredients) }
+  it { should have_and_belong_to_many(:categories) }
 end
