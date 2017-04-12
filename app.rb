9aeacf35bf76a8fe3a90f752d28630e7be38efe9
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-04-12 16:19:18 +0800

    add the admin adn form pages

--- app.rb
+++ app.rb
@@ -5,5 +5,57 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 also_reload("lib/*.rb")
 
 get('/') do
+  @recipes = Recipe.all()
   erb(:index)
+end
+
+delete('/database_reset') do
+  @categories = Category.all()
+  @ingredients = Ingredient.all()
+  @instructions = Instruction.all()
+  @recipes = Recipe.all()
+  @categories.each() do |category|
+    category.destroy()
+  end
+  @instructions.each() do |instruction|
+    instruction.destroy()
+  end
+  @ingredients.each() do |ingredient|
+    ingredient.destroy()
+  end
+  @recipes.each() do |recipe|
+    recipe.destroy()
+  end
+  redirect('/')
+end
+
+post('/') do
+  recipe_name = params.fetch('name')
+  ingredient_name1 = params.fetch('ingredient1')
+  instruction_name1 = params.fetch('instruction1')
+  ingredient_name2 = params.fetch('ingredient2')
+  instruction_name2 = params.fetch('instruction2')
+  ingredient_name3 = params.fetch('ingredient3')
+  instruction_name3 = params.fetch('instruction3')
+  ingredient_name4 = params.fetch('ingredient4')
+  instruction_name4 = params.fetch('instruction4')
+  recipe = Recipe.create({:name => recipe_name})
+  ingredient1 = Ingredient.create({:name => ingredient_name1, :recipe_id => recipe.id()})
+  instruction1 = Instruction.create({:description => instruction_name1, :recipe_id => recipe.id()})
+  ingredient2 = Ingredient.create({:name => ingredient_name2, :recipe_id => recipe.id()})
+  instruction2 = Instruction.create({:description => instruction_name2, :recipe_id => recipe.id()})
+  ingredient3 = Ingredient.create({:name => ingredient_name3, :recipe_id => recipe.id()})
+  instruction3 = Instruction.create({:description => instruction_name3, :recipe_id => recipe.id()})
+  ingredient4 = Ingredient.create({:name => ingredient_name4, :recipe_id => recipe.id()})
+  instruction4 = Instruction.create({:description => instruction_name4, :recipe_id => recipe.id()})
+  redirect("/")
+end
+
+get('/recipes/new') do
+  erb(:form)
+end
+
+get('/recipes/:id') do
+  @recipe = Recipe.find(params.fetch('id'))
+  erb(:recipe)
 end
--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -4,4 +4,10 @@ describe(Recipe) do
   it { should have_many(:instructions) }
   it { should have_many(:ingredients) }
   it { should have_and_belong_to_many(:categories) }
+
+  it("capitalizes each word in a recipe name") do
+    test_recipe = Recipe.create({:name => "fRIED pOTATOES", :rating => nil})
+    expect(test_recipe.name()).to(eq('Fried Potatoes'))
+  end
+  
 end
